161b6da5a221d51d84ef1a79717ee2929da36dff
Clara Munro and Mark Strickland <clara@>
2019-01-22 14:29:41 -0800

    mad libs, done

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,13 @@
 $(document).ready(function() {
   $("#formOne").submit(function(event) {
-    var person1Input = $("input#person1").val();
-    var person2Input = $("input#person2").val();
-    var animalInput= $("input#animal").val();
-    var exclamationInput = $("input#exclamation").val();
-    var verbInput = $("input#verb").val();
-    var nounInput = $("input#noun").val();
+    var fields = ["person1", "person2", "animal", "exclamation", "verb", "noun"];
 
-    $(".person1").text(person1Input);
-    $(".person2").text(person2Input);
-    $(".animal").text(animalInput);
-    $(".exclamation").text(exclamationInput);
-    $(".verb").text(verbInput);
-    $(".noun").text(nounInput);
+    fields.forEach(function(field){
+      var userInput = $("input#" + field).val();
+      $("." + field).text(userInput);
+    })
 
     $("#story").show();
-
-    event.preventDefault();
-  });
-  $("#formLetter").submit(function(event) {
-    var yournameInput = $("input#yourname").val();
-  
-    $(".yourname").text(yournameInput);
-
-    $("#letter").show();
 
     event.preventDefault();
   });
